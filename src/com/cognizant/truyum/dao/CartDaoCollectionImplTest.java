fff37eb74ae050737a6b5146bb767e0c2ac30c7c
prasanth2049 <v.prasanth.20498@gmail.com>
2019-12-23 10:50:58 +0530

    java module

--- src/com/cognizant/truyum/dao/CartDaoCollectionImplTest.java
+++ src/com/cognizant/truyum/dao/CartDaoCollectionImplTest.java
@@ -5,45 +5,55 @@ import java.util.List;
 import com.cognizant.truyum.model.MenuItem;
 
 public class CartDaoCollectionImplTest {
+       public static void main(String[] args) throws CartEmptyException {
+              testGetAllCartItems();
+              testAddCartItem();
+              testGetAllCartItems();
+              testRemoveCartItem();
+              testGetAllCartItems();
 
-	public static void testAddCartItem() throws CartEmptyException {
-		CartDao cartDao = new CartDaoCollectionImpl();
-		cartDao.addCartItem(1, 2L);
-		cartDao.addCartItem(1, 5L);
-		cartDao.addCartItem(2, 1L);
-		cartDao.addCartItem(2, 3L);
-		List<MenuItem> menuItemListCustomer = cartDao.getAllCartItems(1);
-		System.out.println("User Added Cart List for CheckOut");
-		for (MenuItem menuItem : menuItemListCustomer) {
-			System.out.println(menuItem);
-		}
-	}
+       }
 
-	public static void testRemoveCartItem() throws CartEmptyException {
-		CartDao cartDao = new CartDaoCollectionImpl();
-		System.out.println("Item List for Customer after Remove");
-		try {
-			cartDao.removeCartItem(1, 2L);
-			cartDao.removeCartItem(1, 3L);
-			cartDao.removeCartItem(1, 2L);
-			cartDao.removeCartItem(1, 5L);
-			cartDao.removeCartItem(2, 1L);
-			cartDao.removeCartItem(2, 3L);
-			List<MenuItem> menuItemListCustomer = cartDao.getAllCartItems(1);
-			for (MenuItem menuItem : menuItemListCustomer) {
-				System.out.println(menuItem);
-			}
-		} catch (CartEmptyException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+       public static void testAddCartItem() throws CartEmptyException {
+              CartDao cartDao = new CartDaoCollectionImpl();
+              cartDao.addCartItem(1, 2L);
+              cartDao.addCartItem(1, 5L);
+              cartDao.addCartItem(2, 1L);
+              cartDao.addCartItem(2, 3L);
+              List<MenuItem> menuItemListCustomer = cartDao.getAllCartItems(1);
+              System.out.println("User Added Cart List for CheckOut");
+              for (MenuItem menuItem : menuItemListCustomer) {
+                     System.out.println(menuItem);
+              }
+       }
 
-	public static void testGetAllCartItems() {
+       public static void testGetAllCartItems() throws CartEmptyException {
+              CartDao cartDao = new CartDaoCollectionImpl();
+              try {
+                     List<MenuItem> menuItemListCustomer = cartDao.getAllCartItems(1);
+                     System.out.println("User List All Cart Items");
+                     for (MenuItem menuItem : menuItemListCustomer) {
 
-	}
+                           System.out.println(menuItem);
+                     }
+              } catch (CartEmptyException e) {
+                     System.out.println(e.getMessage());
+              }
+       }
 
-	public static void main(String[] args) throws CartEmptyException {
-		testAddCartItem();
-		testRemoveCartItem();
-	}
+       public static void testRemoveCartItem() throws CartEmptyException {
+              CartDao cartDao = new CartDaoCollectionImpl();
+              System.out.println("Item List for Customer after Remove");
+              try {
+                     cartDao.removeCartItem(1, 2L);
+                     List<MenuItem> menuItemListCustomer = cartDao.getAllCartItems(1);
+                     for (MenuItem menuItem : menuItemListCustomer) {
+                           System.out.println(menuItem);
+                     }
+              } catch (CartEmptyException e) {
+                     System.out.println(e.getMessage());
+              }
+       }
 }
+
+
